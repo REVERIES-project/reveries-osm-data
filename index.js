6df8a31da84d082ef7e09ea4617a7d18b6d621bc
gick <gicquel.pierreyves@gmail.com>
2018-12-13 14:49:17 +0100

    allow origin

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var logger = Winston.createLogger({
 // creating express server
 let app = Express()
 app.use(cors({
-    origin:['http://osm.reveries-project.fr','http://localhost:3000'],
+    origin:['http://localhost:3000','http://osm.reveries-project.fr'],
     methods:['GET','POST'],
     credentials: true // enable set cookie
 }
@@ -35,11 +35,6 @@ app.use(BodyParser.urlencoded({
 }))
 app.use(BodyParser.json())
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
   
   
 // attempt to connect to mongodb
